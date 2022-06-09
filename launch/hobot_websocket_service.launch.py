61c24c02d48433c7dd517daea5beb264a6c2333b
kao.zhu <kao.zhu@horizon.ai>
2022-06-09 20:15:07 +0800

    chore(webservice): [cr_id_skip] Add service launch file
    Summary: chore(webservice): [cr_id_skip] Add service launch note
    
    Test Plan: verified
    
    Reviewers: zhuo.wang
    
    Reviewed By: zhuo.wang
    
    Differential Revision: https://cr.hobot.cc/D184340

---
+++ launch/hobot_websocket_service.launch.py
@@ -0,0 +1,64 @@
+import os
+import stat
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import TextSubstitution
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_prefix
+
+"""
+功能：
+检查设备上是否已启动nginx服务，
+如果未启动，启动websocket package中的nginx服务后程序退出，
+如果已启动，程序直接退出。
+
+
+使用：
+使用方法1，直接启动此launch文件：
+ros2 launch websocket hobot_websocket_service.launch.py
+运行结束后，程序退出。
+
+
+使用方法2，在另一个launch文件中引用：
+1、创建include description
+    web_service_launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('websocket'),
+                'launch/hobot_websocket_service.launch.py'))
+    )
+
+2、将创建include description作为node添加到LaunchDescription
+    return LaunchDescription([
+        web_service_launch_include,
+        user_defined_node
+    ])
+"""
+
+def generate_launch_description():
+    # 启动webserver服务
+    name = 'nginx'
+    launch_webserver = True
+    for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
+        launch_webserver = False
+        break
+
+    if launch_webserver:
+        print("launch webserver")
+        webserver_path = os.path.join(get_package_prefix('websocket'),
+                                    "lib/websocket/webservice")
+        print("webserver_path is ", webserver_path)
+        os.chdir(webserver_path)
+        nginx = "./sbin/" + name
+        os.chmod(nginx, stat.S_IRWXU)
+        webserver = nginx + " -p ."
+        print("launch webserver cmd is ", webserver)
+        os.system(webserver)
+    else:
+        print("webserver has launch")
+
+    return LaunchDescription([
+            ])
