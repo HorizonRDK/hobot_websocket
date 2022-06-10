7f5dfa5462bd82748ba04ee6a189832f20089b47
zhuo.wang <zhuo.wang@horizon.ai>
2022-06-10 15:18:45 +0800

    chore(websocket): [cr_id_skip] Add License and Copyright no_3rd_skip
    Test Plan: verify
    
    Reviewers: kairui.wang, kao.zhu
    
    Reviewed By: kao.zhu
    
    Subscribers: zhuo.wang
    
    Differential Revision: https://cr.hobot.cc/D184657

--- include/utils/time_helper.h
+++ include/utils/time_helper.h
@@ -1,13 +1,17 @@
-/*!
- * Copyright (c) 2016-present, Horizon Robotics, Inc.
- * All rights reserved.
- * \File     helpers.h
- * \Author   Yingmin Li
- * \Mail     yingmin.li-horizon.ai
- * \Version  1.0.0.0
- * \Date     2019/1/10
- * \Brief    implement of helpers header
- */
+// Copyright (c) 2022，Horizon Robotics.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef TIME_HELPERS_H_
 #define TIME_HELPERS_H_
 
